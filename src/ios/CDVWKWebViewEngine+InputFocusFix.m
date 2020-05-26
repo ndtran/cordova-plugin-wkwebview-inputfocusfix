6f1a9fbe2b3dc6df634730c4ab39202dcd7992f7
Nicolas Tran <nox.tran@gmail.com>
2020-05-26 14:22:13 +0200

    Update CDVWKWebViewEngine+InputFocusFix.m

--- src/ios/CDVWKWebViewEngine+InputFocusFix.m
+++ src/ios/CDVWKWebViewEngine+InputFocusFix.m
@@ -38,7 +38,13 @@
     }
     
     else if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_12_2_0]) {
-        methodSignature = "_elementDidFocus:userIsInteracting:blurPreviousNode:changingActivityState:userObject:";
+        SEL selector = sel_getUid("_elementDidFocus:userIsInteracting:blurPreviousNode:changingActivityState:userObject:");
+        Method method = class_getInstanceMethod(class, selector);
+        IMP original = method_getImplementation(method);
+        IMP override = imp_implementationWithBlock(^void(id me, void* arg0, BOOL arg1, BOOL arg2, BOOL arg3, id arg4) {
+        ((void (*)(id, SEL, void*, BOOL, BOOL, BOOL, id))original)(me, selector, arg0, TRUE, arg2, arg3, arg4);
+        });
+        method_setImplementation(method, override);
     }
     
     else if ([[NSProcessInfo processInfo] isOperatingSystemAtLeastVersion: iOS_11_3_0]) {
